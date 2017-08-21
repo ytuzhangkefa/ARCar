9534693e0a6d1b0c68816b4d4ed3eccc9129b00c
ytuzhangkefa <122818127@qq.com>
2017-08-21 11:04:57 +0800

    * HelloWorld

--- Test/Assets/HelloWorld.cs
+++ Test/Assets/HelloWorld.cs
@@ -8,7 +8,7 @@ public class HelloWorld : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        print("Hello World");	
+        print("Hello World 世界你不好");	
 	}
 	
 	// Update is called once per frame
